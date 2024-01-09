a3d0f5a9c4a67c6e62b49f27bac10bce68811ee1
EngineerLucky <lucksehlatshwayo19@gmail.com>
2024-01-09 06:06:07 -0800

    betty.c

---
+++ betty.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - The entry point of the program
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int v;
+
+	printf("Hello, Lucky!\n");
+
+	for (v = 0; v < 10; v++)
+	{
+		printf("Iteration %d\n", v);
+	}
+
+	return (0);
+}
---
+++ builtin.c
@@ -0,0 +1,97 @@
+#include "shell.h"
+
+/**
+ * _exiting - The shell must be exited by the exit
+ * @info: The Structures containing potential arguments, must be used
+ * in constant function prototype.
+ * Return: exits status should be given
+ * (0) if info.argv[0] != "exit"
+ */
+int _exiting(info_t *info)
+{
+	int exitcheck;
+
+	if (info->argv[1])  /* when there is an exit arguement */
+	{
+		exitcheck = _erratoi(info->argv[1]);
+		if (exitcheck == -1)
+		{
+			info->status = 2;
+			print_error(info, "Illegal number: ");
+			_eputs(info->argv[1]);
+			_eputchar('\n');
+			return (1);
+		}
+		info->err_num = _erratoi(info->argv[1]);
+		return (-2);
+	}
+	info->err_num = -1;
+	return (-2);
+}
+
+/**
+ * _forcd - For the directory of the process that should be changed
+ * @info: the Structure containing potential arguments must be  Used to mentan
+ * constant function prototype.
+ * Return: Always 0
+ */
+int _forcd(info_t *info)
+{
+	char *s, *dir, buffer[1024];
+	int chdir_ret;
+
+	s = getcwd(buffer, 1024);
+	if (!s)
+		_puts("TODO: >>getcwd failure emsg here<<\n");
+	if (!info->argv[1])
+	{
+		dir = _getenv(info, "HOME=");
+		if (!dir)
+			chdir_ret = /* TODO: what should this be? */
+				chdir((dir = _getenv(info, "PWD=")) ? dir : "/");
+		else
+			chdir_ret = chdir(dir);
+	}
+	else if (_strcmp(info->argv[1], "-") == 0)
+	{
+		if (!_getenv(info, "OLDPWD="))
+		{
+			_puts(s);
+			_putchar('\n');
+			return (1);
+		}
+		_puts(_getenv(info, "OLDPWD=")), _putchar('\n');
+		chdir_ret = /* TODO: what should this be? */
+			chdir((dir = _getenv(info, "OLDPWD=")) ? dir : "/");
+	}
+	else
+		chdir_ret = chdir(info->argv[1]);
+	if (chdir_ret == -1)
+	{
+		print_error(info, "can't cd to ");
+		_eputs(info->argv[1]), _eputchar('\n');
+	}
+	else
+	{
+		_setenv(info, "OLDPWD", _getenv(info, "PWD="));
+		_setenv(info, "PWD", getcwd(buffer, 1024));
+	}
+	return (0);
+}
+
+/**
+ * _ourhelp - current directory of the process should change
+ * @info: the Structure containing potential arguments that are Used to manten
+ * constant function prototype.
+ * Return: Always 0
+ */
+int _ourhelp(info_t *info)
+{
+	char **arg_array;
+
+	arg_array = info->argv;
+	_puts("help call works. Function not yet implemented \n");
+	if (0)
+		_puts(*arg_array); /* temp att_unused workaround */
+	return (0);
+}
